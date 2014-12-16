f109155fada4f4a5ae4d21a9ceb0fdc0b4c22c99
barisser <barisser@gmail.com>
2014-12-15 19:03:09 -0800

    fix

--- assemblycoins/push.py
+++ assemblycoins/push.py
@@ -4,9 +4,9 @@ import os
 def send_btc(from_addr, from_private, destination_address, btc_value):
      fee=os.environ['STANDARD_BTC_FEE']
 
-     tx=transactions.make_raw_transaction(public_address,amount,destination, fee)
+     tx=transactions.make_raw_transaction(from_addr,btc_value,destination_address, fee)
      try:
-         tx2=transactions.sign_tx(tx, private_key)
+         tx2=transactions.sign_tx(tx, from_private)
      except:
          tx2=''
      tx3=transactions.pushtx(tx2)
